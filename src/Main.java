85082ea13474790dcbd80ca32c67ec306510aa62
fleiscjl <113860663+fleiscjl@users.noreply.github.com>
2022-09-20 13:57:45 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+     System.out.println();
+
+     int springCost = 1500;
+     int summerCost = 2000;
+     int fallCost = 1500;
+     int winterCost = 2000;
+     int totalCost;
+     totalCost = springCost + summerCost + fallCost + winterCost;
+     System.out.println("Your yearly maintenance cost is " + totalCost);
     }
 }
+
